68c5d5cfbac3e6dedb2f26238ed6a81527e795d0
tungphongdo89 <tungphongdo89@gmail.com>
2023-11-20 13:35:58 +0700

    Update new api

---
+++ src/main/java/com/tungphongdo/entity/Person.java
@@ -0,0 +1,25 @@
+package com.tungphongdo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ADDRESS")
+    private String address;
+}
