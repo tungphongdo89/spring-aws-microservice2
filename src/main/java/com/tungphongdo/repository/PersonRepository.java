01c09640e4eaf6549dd0a10965d94b4849f7aaa6
tungphongdo89 <tungphongdo89@gmail.com>
2023-11-20 14:53:08 +0700

    Remove connect DB

--- src/main/java/com/tungphongdo/repository/PersonRepository.java
+++ src/main/java/com/tungphongdo/repository/PersonRepository.java
@@ -1,9 +1,9 @@
-package com.tungphongdo.repository;
-
-import com.tungphongdo.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
+//package com.tungphongdo.repository;
+//
+//import com.tungphongdo.entity.Person;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface PersonRepository extends JpaRepository<Person, Integer> {
+//}
