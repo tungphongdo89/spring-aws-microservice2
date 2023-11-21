d32de7c1448f0b9f58eca6ea89b27047d3b19b47
tungphongdo89 <tungphongdo89@gmail.com>
2023-11-21 11:00:36 +0700

    Add connection DB

--- src/main/java/com/tungphongdo/controller/PersonController.java
+++ src/main/java/com/tungphongdo/controller/PersonController.java
@@ -1,41 +1,41 @@
-//package com.tungphongdo.controller;
-//
-//import com.tungphongdo.entity.Person;
-//import com.tungphongdo.service.PersonService;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping(value = {"/person"})
-//@Slf4j
-//public class PersonController {
-//
-//    private final PersonService personService;
-//
-//
-//    @GetMapping
-//    public ResponseEntity<List<Person>> getAllPersons(){
-//        return ResponseEntity.ok(personService.getAllPersons());
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Person> getPersonById(@PathVariable Integer id){
-//        return ResponseEntity.ok(personService.getPersonById(id));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<Person> createPerson(@RequestBody Person person){
-//        return ResponseEntity.ok(personService.createPerson(person));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<String> deletePersonById(@PathVariable Integer id){
-//        return ResponseEntity.ok(personService.deletePersonById(id));
-//    }
-//
-//}
+package com.tungphongdo.controller;
+
+import com.tungphongdo.entity.Person;
+import com.tungphongdo.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = {"/person"})
+@Slf4j
+public class PersonController {
+
+    private final PersonService personService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Person>> getAllPersons(){
+        return ResponseEntity.ok(personService.getAllPersons());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> getPersonById(@PathVariable Integer id){
+        return ResponseEntity.ok(personService.getPersonById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+        return ResponseEntity.ok(personService.createPerson(person));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePersonById(@PathVariable Integer id){
+        return ResponseEntity.ok(personService.deletePersonById(id));
+    }
+
+}
--- src/main/java/com/tungphongdo/service/PersonService.java
+++ src/main/java/com/tungphongdo/service/PersonService.java
@@ -1,32 +1,32 @@
-//package com.tungphongdo.service;
-//
-//import com.tungphongdo.entity.Person;
-//import com.tungphongdo.repository.PersonRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class PersonService {
-//
-//    private final PersonRepository personRepository;
-//
-//    public List<Person> getAllPersons(){
-//        return personRepository.findAll();
-//    }
-//
-//    public Person getPersonById(Integer id){
-//        return personRepository.findById(id).orElseThrow();
-//    }
-//
-//    public Person createPerson(Person person){
-//        return personRepository.save(person);
-//    }
-//
-//    public String deletePersonById(Integer id){
-//        personRepository.deleteById(id);
-//        return "Deleted person successfully";
-//    }
-//}
+package com.tungphongdo.service;
+
+import com.tungphongdo.entity.Person;
+import com.tungphongdo.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PersonService {
+
+    private final PersonRepository personRepository;
+
+    public List<Person> getAllPersons(){
+        return personRepository.findAll();
+    }
+
+    public Person getPersonById(Integer id){
+        return personRepository.findById(id).orElseThrow();
+    }
+
+    public Person createPerson(Person person){
+        return personRepository.save(person);
+    }
+
+    public String deletePersonById(Integer id){
+        personRepository.deleteById(id);
+        return "Deleted person successfully";
+    }
+}
